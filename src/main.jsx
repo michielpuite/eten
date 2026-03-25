779f7e0987b926fd3ad70df72928eba1c829556c
michielpuite <michielpuite@gmail.com>
2026-03-25 11:40:08 +0100

    Refactor main.jsx for root element and logging
    Refactor main.jsx to improve root element handling and add logging.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import app from './app.jsx'
+import App from './app.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <app />
-  </React.StrictMode>,
-)
+// We zoeken het 'root' element uit je index.html
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log("React is succesvol geladen in de root!");
+} else {
+  console.error("Fout: Kon het element met id 'root' niet vinden in de HTML.");
+}
